3efe32b2040bceb0fce8d21de87707320198f14e
Hassan El-Sheikha <71786895+hassanelsheikha@users.noreply.github.com>
2022-04-25 18:08:59 -0400

    added 404 and about page

--- __init__.py
+++ __init__.py
@@ -177,5 +177,15 @@ def about_tower_of_hanoi():
     return render_template("about_hanoi.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run()
